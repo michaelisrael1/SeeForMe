3b55193042148d9808a176b4f59323d8f83b5c41
Michael Israel <michaelisrael@unomaha.edu>
2025-10-13 14:14:51 -0500

    Added Rekognition image class

---
+++ image.py
@@ -0,0 +1,45 @@
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+class RekognitionImage:
+    """
+    Encapsulates an Amazon Rekognition image. This class is a thin wrapper
+    around parts of the Boto3 Amazon Rekognition API.
+    """
+
+    def __init__(self, image, image_name, rekognition_client):
+        """
+        Initializes the image object.
+
+        :param image: Data that defines the image, either the image bytes or
+                      an Amazon S3 bucket and object key.
+        :param image_name: The name of the image.
+        :param rekognition_client: A Boto3 Rekognition client.
+        """
+        self.image = image
+        self.image_name = image_name
+        self.rekognition_client = rekognition_client
+
+    def detect_labels(self, max_labels, min_confidence):
+        """
+        Detects labels in the image. Labels are objects and people.
+
+        :param max_labels: The maximum number of labels to return.
+        :return: The list of labels detected in the image.
+        """
+        try:
+            response = self.rekognition_client.detect_labels(
+                Image=self.image, MaxLabels=max_labels,
+                MinConfidence=min_confidence
+            )
+            labels = [label for label in response["Labels"]]
+            logger.info("Found %s labels in %s.", len(labels), self.image_name)
+        except ClientError:
+            logger.info("Couldn't detect labels in %s.", self.image_name)
+            raise
+        else:
+            return labels
